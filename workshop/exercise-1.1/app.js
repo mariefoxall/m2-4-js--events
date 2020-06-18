82a4c4053a62edc8c645fb5b18ac6557d6ff8702
Marie Foxall <marie@wastedeffort.ca>
2020-06-18 14:44:26 -0400

    Exercise 1.1

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,30 @@
 // Exercise 1.1
 // ------------
-console.log('exercise 1.1');
+console.log("exercise 1.1");
+
+const body = document.querySelector("body");
+const result = document.querySelector(".result");
+
+let hasWon = false;
+let gameOver = false;
+
+let winner = function () {
+  console.log("click");
+  if (gameOver === false) {
+    hasWon = true;
+    result.innerText = "You win!";
+    body.removeEventListener("click", winner);
+    console.log("winner");
+  }
+};
+
+body.addEventListener("click", winner);
+
+setTimeout(function () {
+  gameOver = true;
+  if (hasWon === false) {
+    result.innerText = "You lose!";
+    body.removeEventListener("click", winner);
+    console.log("loser");
+  }
+}, 5000);
